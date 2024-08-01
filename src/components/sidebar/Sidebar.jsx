79a21cf66cff9b03022ff0d4d31413ae5a9ce65b
zoro-diablo <vyshnavk117@gmail.com>
2024-08-01 19:58:56 +0530

    change on partnerpage

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import {
   Home,
   Settings,
@@ -9,76 +9,76 @@ import {
   BriefcaseBusinessIcon,
   Calculator,
   UsersIcon,
-} from "lucide-react";
+} from 'lucide-react';
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip";
-import logo from "../../assets/logo.svg";
-import { GrTransaction } from "react-icons/gr";
+} from '@/components/ui/tooltip';
+import logo from '../../assets/logo.svg';
+import { GrTransaction } from 'react-icons/gr';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+} from '@/components/ui/dropdown-menu';
 
 function Sidebar() {
   return (
-    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r dark:border-[#555555] dark:bg-black bg-background sm:flex">
+    <aside className='fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r dark:border-[#555555] dark:bg-black bg-background sm:flex'>
       <TooltipProvider>
-        <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+        <nav className='flex flex-col items-center gap-4 px-2 sm:py-5'>
           <NavLink
-            to="/"
-            className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base bg-white shadow-md "
+            to='/'
+            className='group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base bg-white shadow-md '
           >
             <img
               src={logo}
               className={`h-6 p-1  w-8 transition-all group-hover:scale-110 `}
             />
-            <span className="sr-only">FnPersona</span>
+            <span className='sr-only'>FnPersona</span>
           </NavLink>
 
           <Tooltip>
             <TooltipTrigger asChild>
               <NavLink
-                to="/dashboard/lobby"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                to='/dashboard/lobby'
+                className='flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
               >
-                <Home className="h-5 w-5 activebutton" />
-                <span className="sr-only">Dashboard</span>
+                <Home className='h-5 w-5 activebutton' />
+                <span className='sr-only'>Dashboard</span>
               </NavLink>
             </TooltipTrigger>
-            <TooltipContent side="right">Dashboard</TooltipContent>
+            <TooltipContent side='right'>Dashboard</TooltipContent>
           </Tooltip>
 
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <NavLink
-                to="/dashboard/budget"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                to='/dashboard/budget'
+                className='flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
               >
-                <DollarSign className="h-5 w-5 activebutton" />
-                <span className="sr-only">Budget</span>
+                <DollarSign className='h-5 w-5 activebutton' />
+                <span className='sr-only'>Budget</span>
               </NavLink>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56 ml-14 mt-[-35px]">
+            <DropdownMenuContent className='w-56 ml-14 mt-[-35px]'>
               <DropdownMenuGroup>
                 <DropdownMenuItem asChild>
                   <NavLink
-                    to="/dashboard/budget/addbudget"
-                    className="flex items-center px-2 py-2 text-sm dark:text-gray-100 dark:hover:bg-gray-700"
+                    to='/dashboard/budget/addbudget'
+                    className='flex items-center px-2 py-2 text-sm dark:text-gray-100 dark:hover:bg-gray-700'
                   >
                     Add Budget
                   </NavLink>
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild>
                   <NavLink
-                    to="/dashboard/budget/overview"
-                    className="flex items-center px-2 py-2 text-sm dark:text-gray-100 dark:hover:bg-gray-700"
+                    to='/dashboard/budget/overview'
+                    className='flex items-center px-2 py-2 text-sm dark:text-gray-100 dark:hover:bg-gray-700'
                   >
                     Overview Budget
                   </NavLink>
@@ -90,27 +90,27 @@ function Sidebar() {
           <Tooltip>
             <TooltipTrigger asChild>
               <NavLink
-                to="/dashboard/transactions"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                to='/dashboard/transactions'
+                className='flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
               >
-                <GrTransaction className="h-5 w-5 activebutton" />
-                <span className="sr-only">Transactions</span>
+                <GrTransaction className='h-5 w-5 activebutton' />
+                <span className='sr-only'>Transactions</span>
               </NavLink>
             </TooltipTrigger>
-            <TooltipContent side="right">Transactions</TooltipContent>
+            <TooltipContent side='right'>Transactions</TooltipContent>
           </Tooltip>
 
           <Tooltip>
             <TooltipTrigger asChild>
               <NavLink
-                to="/dashboard/recurring"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                to='/dashboard/recurring'
+                className='flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
               >
-                <Calendar className="h-5 w-5 activebutton" />
-                <span className="sr-only">Recurring Expenses</span>
+                <Calendar className='h-5 w-5 activebutton' />
+                <span className='sr-only'>Recurring Expenses</span>
               </NavLink>
             </TooltipTrigger>
-            <TooltipContent side="right">Recurring Expenses</TooltipContent>
+            <TooltipContent side='right'>Recurring Expenses</TooltipContent>
           </Tooltip>
 
           <DropdownMenu>
@@ -148,27 +148,27 @@ function Sidebar() {
           <Tooltip>
             <TooltipTrigger asChild>
               <NavLink
-                to="/dashboard/retirement"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                to='/dashboard/retirement'
+                className='flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
               >
-                <Calculator className="h-5 w-5 activebutton" />
-                <span className="sr-only">RetirementPlanner</span>
+                <Calculator className='h-5 w-5 activebutton' />
+                <span className='sr-only'>RetirementPlanner</span>
               </NavLink>
             </TooltipTrigger>
-            <TooltipContent side="right">RetirementPlanner</TooltipContent>
+            <TooltipContent side='right'>RetirementPlanner</TooltipContent>
           </Tooltip>
 
           <Tooltip>
             <TooltipTrigger asChild>
               <NavLink
-                to="/dashboard/investments"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                to='/dashboard/investments'
+                className='flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
               >
-                <BriefcaseBusinessIcon className="h-5 w-5 activebutton" />
-                <span className="sr-only">Investments</span>
+                <BriefcaseBusinessIcon className='h-5 w-5 activebutton' />
+                <span className='sr-only'>Investments</span>
               </NavLink>
             </TooltipTrigger>
-            <TooltipContent side="right">Investments</TooltipContent>
+            <TooltipContent side='right'>Investments</TooltipContent>
           </Tooltip>
 
           <Tooltip>
@@ -194,21 +194,21 @@ function Sidebar() {
                 <span className='sr-only'>Net Worth</span>
               </NavLink>
             </TooltipTrigger>
-            <TooltipContent side='right'>Manage Partnership</TooltipContent>
-          </Tooltip>
+            <TooltipContent side='right'>Net Worth</TooltipContent>
+          </Tooltip> */}
         </nav>
-        <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+        <nav className='mt-auto flex flex-col items-center gap-4 px-2 sm:py-5'>
           <Tooltip>
             <TooltipTrigger asChild>
               <NavLink
-                to="/dashboard/settings"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                to='/dashboard/settings'
+                className='flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
               >
-                <Settings className="h-5 w-5 activebutton" />
-                <span className="sr-only">Settings</span>
+                <Settings className='h-5 w-5 activebutton' />
+                <span className='sr-only'>Settings</span>
               </NavLink>
             </TooltipTrigger>
-            <TooltipContent side="right">Settings</TooltipContent>
+            <TooltipContent side='right'>Settings</TooltipContent>
           </Tooltip>
         </nav>
       </TooltipProvider>
