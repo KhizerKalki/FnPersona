8ff22ce44f7c687c00dc18465f02da0fd1c94fc6
Krishna Saxena <rio17105@gmail.com>
2024-08-06 13:04:43 +0530

    combined overview and add budget sections

--- src/routes/dashboard/Overviews.jsx
+++ src/routes/dashboard/Overviews.jsx
@@ -1,17 +1,30 @@
-import BudgetCards from '@/components/budget/BudgetCards';
-import NotificationTwo from '@/components/budget/NotificationTwo';
-import { BudgetBarGraph } from '@/components/graph/client/BudgetBarGraph';
-import BudgetDistri from '@/components/graph/client/BudgetDistri';
-import BudgetExp from '@/components/graph/client/BudgetExp';
+import BudgetCards from "@/components/budget/BudgetCards";
+import NotificationTwo from "@/components/budget/NotificationTwo";
+import { BudgetBarGraph } from "@/components/graph/client/BudgetBarGraph";
+import BudgetDistri from "@/components/graph/client/BudgetDistri";
+import BudgetExp from "@/components/graph/client/BudgetExp";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 const Overviews = () => {
+  const navigate = useNavigate();
   return (
-    
-      <div className="container mx-auto p-4">
-      <div className="text-2xl font-semibold mt-5 dark:text-white">Budget Overview</div>
-      <p className="text-muted-foreground mb-4 mt-1 dark:text-white/50">
-        Get a clear snapshot of your spending and savings.
-      </p>
+    <div className="container mx-auto p-4">
+      <div className="flex justify-between">
+        <div>
+          <div className="text-2xl font-semibold mt-5 dark:text-white">
+            Budget Overview
+          </div>
+          <p className="text-muted-foreground mb-4 mt-1 dark:text-white/50">
+            Get a clear snapshot of your spending and savings.
+          </p>
+        </div>
+        <div className="mt-10">
+          <Button onClick={() => navigate("/dashboard/budget/addbudget")}>
+            Add Budget
+          </Button>
+        </div>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         <div className="col-span-1 sm:col-span-2 lg:col-span-4"></div>
         <div className="col-span-1">
@@ -30,7 +43,7 @@ const Overviews = () => {
           <NotificationTwo />
         </div>
       </div>
-    </div>  
+    </div>
   );
 };
 
